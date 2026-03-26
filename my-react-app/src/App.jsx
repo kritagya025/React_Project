d16bfc14f86731f0ca721147c479bb3ac3069d18
Prajwalwho <prajwalsingh1376@gmail.com>
2026-03-26 12:38:20 +0530

    feat: added contact page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import About from './components/about';
 import Home from './components/Home';
+import Contact from './components/Contact';
 import { BrowserRouter, Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -13,11 +14,13 @@ function App() {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         
       </Routes>
     </div>
