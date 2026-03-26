80ea5da5563c50a338c352847f813a44aabcef38
Krishna Garg <01krishhnnaa@gmail.com>
2026-03-26 12:26:17 +0530

    added gibrish

--- my-react-app/src/components/Home.jsx
+++ my-react-app/src/components/Home.jsx
@@ -3,6 +3,7 @@ function Home() {
     <div>
       <h2>Welcome to the Home Page</h2>
       <p>This is the main dashboard of our community.</p>
+      <h1>remove this later</h1>
     </div>
   );
 }
