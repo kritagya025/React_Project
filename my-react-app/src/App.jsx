d65520d755e0086923f500789b8e88dd597c26c2
Kritagya Yadav <kritagyay2006@gmail.com>
2026-03-26 11:43:00 +0530

    feat: Initialize new React application with core dependencies and project structure.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import './App.css';
 import About from './components/about';
+import Home from './components/Home';
+import { BrowserRouter, Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="container">
       <header className="header">
         <h1>DevConnect</h1>
         <nav>
-          <a href="#home">Home</a>
-          <a href="about.jsx">About</a>
-          <a href="#contact">Contact</a>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
         </nav>
-      
       </header>
-<About />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ my-react-app/src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to the Home Page</h2>
+      <p>This is the main dashboard of our community.</p>
+    </div>
+  );
+}
+
+export default Home;
